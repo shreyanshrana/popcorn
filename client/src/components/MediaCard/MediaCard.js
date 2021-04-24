c70624bafa1af690937688fe1248238d941a8b0a
Shreyansh <shreyanshrana@gmail.com>
2021-04-24 14:20:37 +0530

    added Manrope font:

---
+++ client/src/components/MediaCard/MediaCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MediaCard = () => {
+    return (
+        <React.Fragment>
+            hello 123
+        </React.Fragment>
+    )
+}
+
+export default MediaCard
--- client/src/pages/List/List.js
+++ client/src/pages/List/List.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import MediaCard from '../../components/MediaCard/MediaCard';
 import "./List.css";
 
 const List = () => {
     return (
         <div>
-            list
+            <MediaCard/>
         </div>
     )
 }
