4a044c578c6ec09ae7267712068b305b0d748249
Shreyansh <shreyanshrana@gmail.com>
2021-04-25 16:13:24 +0530

    added width for sidebar

--- client/src/pages/List/List.js
+++ client/src/pages/List/List.js
@@ -9,6 +9,7 @@ const List = (props) => {
     const [trending, setTrending] = useState([]);
 
     useEffect(()=>{
+        document.getElementById("movieList").style.width = window.screen.width - 176 + 'px';
         async function fetchData(){
             const API_URL = "https://api.themoviedb.org/3/trending/movie/week?api_key=6973dcfea19d8ef259f70d85b99207b4";
             const res = await fetch(API_URL);
@@ -25,14 +26,26 @@ const List = (props) => {
                 ?
                 (
                 <React.Fragment>
-                    <h1>Trending this week</h1>
-                    <ListByTrending genreList={props.genreList}/>
-                    <ListByCategory genreList={props.genreList}/>
+                    <div className="w-44 float-left inline-block">
+                        <h1>hello</h1>
+                    </div>
+                    <div className="inline-block" id="movieList">
+                        <h1>Trending this week</h1>
+                        <ListByTrending genreList={props.genreList}/>
+                        <ListByCategory genreList={props.genreList}/>
+                    </div>                    
                 </React.Fragment>
                 )
                 :
                 (
-                    <React.Fragment/>
+                <React.Fragment>
+                    <div className="w-44 float-left inline-block">
+                        
+                    </div>
+                    <div className="inline-block" id="movieList">
+                        
+                    </div>                    
+                </React.Fragment>
                 )
             }
             
